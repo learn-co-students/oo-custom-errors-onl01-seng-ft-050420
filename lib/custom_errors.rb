0975e9bcfe241266b2cf80c94d372f15853c1595
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-20 18:11:44 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -11,7 +11,7 @@ class Person
       begin
         raise PartnerError
       rescue PartnerError =>
-        puts.error.message
+        puts error.message
       end
     else
       person.partner = self
